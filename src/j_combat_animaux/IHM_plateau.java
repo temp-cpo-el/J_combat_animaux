04c3d3b38040604ec000dd6f5f9d6c88c4c29702
annag <annag@192.168.1.79>
2021-02-17 14:36:23 +0100

    augmentation de la taille du plateau et ajoustement de la possition du tigre

--- src/j_combat_animaux/IHM_plateau.java
+++ src/j_combat_animaux/IHM_plateau.java
@@ -18,7 +18,7 @@ import javax.imageio.ImageIO;
  */
 public class IHM_plateau extends javax.swing.JFrame {
 
-    private File fichierplateau = new File("src/images/plateau1.png");//on va chercher le fichier dans le dossier d'images
+    private File fichierplateau = new File("src/images/plateau3.png");//on va chercher le fichier dans le dossier d'images
     private BufferedImage imagePlateau;//la placer en tant que bufferedImage permet de la redessiner à chaque coup
     private File fichierfondplateau = new File("src/images/Fond_plateau_de_jeu0.png");//on va chercher le fichier dans le dossier d'images
     private BufferedImage imageFondPlateau;
@@ -65,7 +65,7 @@ public class IHM_plateau extends javax.swing.JFrame {
     Animal a12=new Animal("chien", 120, 539,0,0, 4,false,false);
     Animal a13=new Animal("panthère", 225, 433,0,0, 5,false,false);
     Animal a14=new Animal("lion", 14, 644,0,0, 6,false,false);
-    Animal a15=new Animal("tigre", 14, 14,0,0, 7,false,false);
+    Animal a15=new Animal("tigre", 290, 162,0,0, 7,false,false);
     Animal a16=new Animal("elephant", 225, 20,0,0, 8,false,false);
     
         ajouterAnimal(a1);
@@ -140,6 +140,7 @@ public class IHM_plateau extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -179,6 +180,8 @@ public class IHM_plateau extends javax.swing.JFrame {
         jButton3.setForeground(new java.awt.Color(255, 255, 255));
         jButton3.setText("QUITTER");
 
+        jLabel1.setText("jLabel1");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -189,11 +192,15 @@ public class IHM_plateau extends javax.swing.JFrame {
                 .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 545, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 410, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(768, Short.MAX_VALUE)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 632, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -273,6 +280,7 @@ private int xtemp, ytemp;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 /* Ajout des toutes les fonctions dont on aura besoin pour faire marcher le code :*/
